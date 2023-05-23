4ce37325a837d377857191a8e762a2cd3ea51a5f
Gabriel <gabz@MacBook-Pro-de-Gabriel.local>
2023-05-22 22:51:19 -0300

    atualizando

---
+++ exercicios_extras/numeros_pares.py
@@ -0,0 +1,9 @@
+lista_numeros = []
+while len(lista_numeros) <= 4:
+    entrada = int(input('Informe um numero: '))
+    lista_numeros.append(entrada)
+    print(lista_numeros)
+
+
+    #if entrada % 2 == 0:
+     #   print(lista_numeros)
