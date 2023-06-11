9651344326c21ab1c1bcad26ba76ff0b1d480cf2
GabzWindows <roochagaabriel@gmail.com>
2023-06-11 01:14:03 -0300

    adicionando aula de conta bancaria

---
+++ Poo/conta.py
@@ -0,0 +1,10 @@
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print(f"Constuindo Objeto...{self}")
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limte = limite
+
+    def extrato(self):
+        print(f"O saldo de R${self.saldo} do titular {self.titular}")
