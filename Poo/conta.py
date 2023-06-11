7ec678eefcfafcf3a4984d257dc52a58cd4dcb17
GabzWindows <roochagaabriel@gmail.com>
2023-06-11 20:01:20 -0300

    atualizando aula banco

--- Poo/conta.py
+++ Poo/conta.py
@@ -4,7 +4,7 @@ class Conta:
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
-        self.__limte = limite
+        self.__limite = limite
 
     def extrato(self):
         print(f"O saldo de R${self.__saldo} do titular {self.__titular}")
@@ -18,3 +18,15 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
