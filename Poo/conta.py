e34c999e68127f1b6d876502ac2e8e0d837dba61
GabzWindows <roochagaabriel@gmail.com>
2023-06-11 01:37:42 -0300

    adicionando aula de conta bancaria

--- Poo/conta.py
+++ Poo/conta.py
@@ -8,3 +8,9 @@ class Conta:
 
     def extrato(self):
         print(f"O saldo de R${self.saldo} do titular {self.titular}")
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
