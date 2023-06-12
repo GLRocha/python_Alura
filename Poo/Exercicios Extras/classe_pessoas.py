8ef4ac62486d0d100b62e7de11bb2d2aa8dba76a
GabzWindows <roochagaabriel@gmail.com>
2023-06-11 23:18:52 -0300

    exercicios extras

---
+++ Poo/Exercicios Extras/classe_pessoas.py
@@ -0,0 +1,10 @@
+"""Crie uma classe chamada "Pessoa" que tenha os atributos "nome" e "idade".
+Em seguida, crie um objeto dessa classe
+e imprima os valores dos atributos.
+"""
+
+
+class Pessoas:
+    def __init__(self, nome, idade):
+        self.__nome = nome
+        self.__idade = idade
